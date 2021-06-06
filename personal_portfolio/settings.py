2c0b21f4f74e4ef37c208069a3459192108049eb
andre <78390294+ankrudov@users.noreply.github.com>
2021-06-06 04:10:23 -0500

    added a new home page,about me. added responsive bootstrap elements collapsible cards and accordion. finishing experience card

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -120,6 +121,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'img')
+MEDIA_URL = '/img/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static'),
+    '/projects/static/img',
+]
+
+
 
 #email settings
 
--- projects/urls.py
+++ projects/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("", views.project_index, name="project_index"),
     path("<int:pk>/", views.project_detail, name="project_detail"),
     path("ContactMe", views.contact_me, name='contact_me'),
+    path("home/",views.home, name='home')
 ]
